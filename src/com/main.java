bd48d81cba4f9a0d071ba5f1344234ef60f9343c
computer <mcgs_wu@163.com>
2019-02-15 11:43:36 +0800

    台式机提交

--- src/com/main.java
+++ src/com/main.java
@@ -3,6 +3,11 @@ package com;
 public class main {
     public static void main(String[] args) {
         System.out.println("11");
+        aa();
+    }
+
+    public static void aa(){
+        System.out.println("33");
     }
 
 }
