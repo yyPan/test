d95f03e057bd06e3cc16267ad0b41d656dcf3564
yyPan <klzy_wlg@163.com>
2019-02-15 14:13:33 +0800

    1413

--- src/com/main.java
+++ src/com/main.java
@@ -4,10 +4,14 @@ public class main {
     public static void main(String[] args) {
         System.out.println("11");
         aa();
+        aaa();
     }
 
     public static void aa(){
         System.out.println("33");
     }
 
+    public static void aaa(){
+        System.out.println("33");
+    }
 }
